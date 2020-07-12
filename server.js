01007871404b8674ccaec624409b5c8a368dd53f
David <David@DESKTOP-UFOBKID>
2020-07-12 02:40:54 +0200

    build prod

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const ngApp = express();
+ngApp.use(express.static('./dist/angular-heroku-test'));
+ngApp.get('/*', function (request, response) {
+    response.sendFile(path.join(__dirname, '/dist/angular-heroku-test/index.html'));
+});
+ngApp.listen(process.env.PORT || 7071);
